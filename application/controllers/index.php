689383c0e0fdfa5a8b6d726df46f8b92a9d4f7af
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2015-01-30 00:07:14 -0200

    ++ front, validation, mask and finish form, change path to assets .htaccess

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -4,10 +4,12 @@ class Index extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
+		$this->load->helper('url');
 	}
 
 	public function index()
 	{
+
 
 		//config SEO
 		$seoParams = array(
@@ -23,35 +25,50 @@ class Index extends CI_Controller {
 		//config Page
 		$page['title'] = 'Formulário de cadastro Encontro com Deus';
 
+
+		$this->load->helper('form');
+
 		$inputNome = array(
 			'name'        => 'nome',
 			'id'          => 'nome',
-			'class'       => 'input-text input-nome'
+			'class'       => 'form-control input-text input-nome',
+			'placeholder' => 'Nome Completo'
         );
 		$inputEmail = array(
 			'name'        => 'email',
 			'id'          => 'email',
-			'class'       => 'input-text input-email'
+			'class'       => 'form-control input-text input-email',
+			'placeholder' => 'E-mail'
         );
         $inputComunidade = array(
 			'name'        => 'comunidade',
 			'id'          => 'comunidade',
-			'class'       => 'input-text input-comunidade'
+			'class'       => 'form-control input-text input-comunidade',
+			'placeholder' => 'Comunidade'
         );
         $inputTelefoneFixo = array(
 			'name'        => 'telefone_fixo',
 			'id'          => 'telefoneFixo',
-			'class'       => 'input-text input-telefone-fixo'
+			'class'       => 'form-control input-text input-telefone-fixo',
+			'placeholder' => 'Telefone Fixo'
         );
         $inputCelular = array(
 			'name'        => 'celular',
 			'id'          => 'celular',
-			'class'       => 'input-text input-celular'
+			'class'       => 'form-control input-text input-celular',
+			'placeholder' => 'Celular'
         );
-        $inputDataNascimento = array(
-			'name'        => 'data_nascimento',
-			'id'          => 'dataNascimento',
-			'class'       => 'input-text input-data-nascimento'
+        $inputPrimeiroRetiro = array(
+			'name'        => 'primeiro_retiro',
+			'id'          => 'primeiroRetiro',
+			'class'       => 'form-control input-text input-celular',
+			'placeholder' => 'Qual foi o seu primeiro retiro?'
+        );
+        $inputObservacoes = array(
+			'name'        => 'observacoes',
+			'id'          => 'observacoes',
+			'class'       => 'form-control input-text input-celular',
+			'placeholder' => 'Por ex: sou intolerante a lactose, sou hipertenso, ou algo do genêro.'
         );
 
         $page['inputNome'] = $inputNome;
@@ -59,29 +76,80 @@ class Index extends CI_Controller {
         $page['inputComunidade'] = $inputComunidade;
         $page['inputTelefoneFixo'] = $inputTelefoneFixo;
         $page['inputCelular'] = $inputCelular;
-        $page['inputDataNascimento'] = $inputDataNascimento;
+        $page['inputPrimeiroRetiro'] = $inputPrimeiroRetiro;
+        $page['inputObservacoes'] = $inputObservacoes;
+
+        $this->load->library('form_validation');
+
+		$this->form_validation->set_rules('nome', 'Nome', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('comunidade', 'Comunidade', 'required');
+		$this->form_validation->set_rules('telefone_fixo', 'Telefone Residencial', 'required');
+		$this->form_validation->set_rules('celular', 'Celular', 'required');
+		$this->form_validation->set_rules('data_nascimento', 'Data de Nascimento', 'required');
+
+		$page['validationError'] = 0;
+
+		if (isset($_POST['nome']) && $this->form_validation->run() == FALSE){
+
+			$page['validationError'] = 1;
+
+		}
+
+		if(isset($_POST['nome'])) {
+
+			if ( $this->cadastro($_POST)) {
+				$page['msgSuccess'] = '<strong> ' . $_POST['nome'] . '</strong> cadastro efetuado com sucesso, muito obrigado!';
+			} else {
+				$page['msgError'] = '<strong> ' . $_POST['nome'] . '</strong> Tudo me leva a crer que você já se cadastrou!';
+			}
+
+		}
 
 		$this->load->view('index', $page);
 
+		
+
 	}
 
-	public function cadastro()
+	public function cadastro($dataForm)
 	{
-		
+
+		$table = 'integrantes';
+
 		$this->load->database();
 
-		$data = array(
-			'nome'				=> $_POST['nome'],
-		    'email' 			=> $_POST['email'],
-		    'comunidade' 		=> $_POST['comunidade'],
-		    'telefone_fixo' 	=> $_POST['telefone_fixo'],
-		    'celular' 			=> $_POST['celular'],
-		    'data_nascimento' 	=> $_POST['data_nascimento']
+		$cadastroExistente = $this->db->query('SELECT email from ' . $table . ' where email = "' . $dataForm['email'] . '"');
+
+		if($cadastroExistente->num_rows() <= 0) {
+
+			$data = array(
+			'nome'				=> $dataForm['nome'],
+		    'email' 			=> $dataForm['email'],
+		    'comunidade' 		=> $dataForm['comunidade'],
+		    'telefone_fixo' 	=> $dataForm['telefone_fixo'],
+		    'celular' 			=> $dataForm['celular'],
+		    'data_nascimento' 	=> $dataForm['data_nascimento'],
+		    'primeiro_retiro' 	=> $dataForm['primeiro_retiro'],
+		    'observacoes' 		=> $dataForm['observacoes']
 			);
 
-		if ($this->db->insert('integrantes', $data)) {
-			echo 'incluiusaporra';
+			if ($this->db->insert($table, $data)) {
+				return true;
+			}
+
 		}
+
+		return false;
+
+
+	}
+
+	public function login()
+	{
+		
+		
+
 	}
 
 }
--- application/views/index.php
+++ application/views/index.php
@@ -1,26 +1,130 @@
-		<div class="all">
+		<div class="jumbotron">
+            <div class="container">
+                <div class="row">                
+                    <h2>Cadastro de participantes</h2>                 
+                </div>
+            </div>
+        </div>
 
-			<h1><?php echo $title; ?></h1>
+        <div class="container">   
+            <div class="row">
+                <div class="col-sm-12">
 
-			<div class="form">
-				<?php 
+                	<div class="row">
+                		<div class="col-sm-12 ">
+                			<?php if(isset($msgSuccess)) { ?>
+	                            <div class="alert alert-success">
+	                                <?php echo $msgSuccess; ?>
+	                                <button id="close-message" type="button" class="close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+	                            </div>
+                			<?php } ?>
+                			<?php if($validationError == 1) { ?>
+                				<div class="alert alert-danger">
+	                                <?php echo validation_errors(); ?>
+	                                <button id="close-message" type="button" class="close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+	                            </div>
+                			<?php } ?>
+                			<?php if(isset($msgError)) { ?>
+                				<div class="alert alert-danger">
+	                                <?php echo $msgError; ?>
+	                                <button id="close-message" type="button" class="close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+	                            </div>
+                			<?php } ?>
+                		</div>
+                	</div>
 
-					$this->load->helper('form');
+                	<div class="row">
 
-					echo form_open('index/cadastro');
+                		<div class="col-sm-2"></div>
+            
+			            <div class="col-sm-8"> 
 
-					echo form_input($inputNome);
-					echo form_input($inputEmail);
-					echo form_input($inputComunidade);
-					echo form_input($inputTelefoneFixo);
-					echo form_input($inputCelular);
-					echo form_input($inputDataNascimento);
+			                <?php 
+			                	$attr = array('class' => 'form-horizontal', 'id' => 'formCadastro', 'role'=>"form");
+			                	echo form_open('index', $attr); 
+			                ?>
+			                    
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">Nome:</label>
+			                        <div class="col-sm-9">
+			                            <?php echo form_input($inputNome); ?>
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">E-mail:</label>
+			                        <div class="col-sm-9">
+			                            <?php echo form_input($inputEmail); ?>
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">Comunidade:</label>
+			                        <div class="col-sm-9">
+			                            <?php echo form_input($inputComunidade); ?>
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">Telefone Fixo:</label>
+			                        <div class="col-sm-9">
+			                            <?php echo form_input($inputTelefoneFixo); ?>
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">Celular:</label>
+			                        <div class="col-sm-9">
+			                            <?php echo form_input($inputCelular); ?>
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">Data de Nascimento:</label>
+			                        <div class="col-sm-9">
+			                            <input type="date" name="data_nascimento" value="" id="dataNascimento" class="form-control input-text input-data-nascimento" placeholder="Data de Nascimento">
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">1ª Participação no ECD:</label>
+			                        <div class="col-sm-9">
+			                            <?php echo form_input($inputPrimeiroRetiro); ?>
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <label for="billName" class="col-sm-3 control-label">Observações:</label>
+			                        <div class="col-sm-9">
+			                            <?php echo form_textarea($inputObservacoes); ?>
+			                        </div>
+			                    </div>
+			                    <div class="form-group">
+			                        <div class="col-sm-9">
+			                    		<?php echo form_submit('enviar', 'Cadastrar!', "class='btn btn-info center-block'"); ?>
+			                        </div>
+			                    </div>
+			                    
+			                
+							<?php echo form_close(); ?>
 
-					echo form_submit('enviar', 'Enviar!');
+			            </div>
+                		
+                	</div>
 
-					echo form_close();
-
-				?>
-			</div>
-			
-		</div>
+                </div>         
+            </div>
+        </div>
+        <script>
+		    jQuery('#formCadastro').validate({
+			rules: {
+				nome: "required",
+				email: "required",
+				comunidade: "required",
+				telefone_fixo: "required",
+				celular: "required",
+				data_nascimento: "required"
+			},
+			messages: {
+				nome: "O campo nome precisa ser preenchido",
+				email: "O campo email precisa ser preenchido",
+				comunidade: "O campo comunidade precisa ser preenchido",
+				telefone_fixo: "O campo telefone_fixo precisa ser preenchido",
+				celular: "O campo celular precisa ser preenchido",
+				data_nascimento: "O campo Data de nascimento precisa ser preenchido"
+			}
+		});
+		</script>
