c3dd7d0e8b5ad331a42643ddb4662682f0dca1a0
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2015-01-30 00:27:49 -0200

    ++ db, update sql

--- application/db/retiros.sql
+++ application/db/retiros.sql
@@ -2,13 +2,16 @@ CREATE DATABASE IF NOT EXISTS `retiros`;
 
 USE `retiros`;
 
-CREATE TABLE IF NOT EXISTS `integrantes` (
+CREATE TABLE `integrantes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nome` varchar(45) DEFAULT NULL,
-  `email` varchar(150) DEFAULT NULL,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(150) NOT NULL,
   `comunidade` varchar(150) NOT NULL,
   `telefone_fixo` varchar(14) NOT NULL,
   `celular` varchar(14) NOT NULL,
   `data_nascimento` date NOT NULL,
+  `primeiro_retiro` varchar(45) DEFAULT NULL,
+  `observacoes` varchar(140) DEFAULT NULL,
+  `senha` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
