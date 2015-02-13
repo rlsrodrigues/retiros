cf1c91fda3402ac48c6bc26889e392319d826ec1
Renato Rodrigues <renato.leandrorodrigues@gmail.com>
2015-02-13 01:09:39 -0200

    ++ crud retiros, js and css scripts

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,3 +8,5 @@ $(document).ready(function(){
 $('#telefoneFixo').mask('(00) 0000-0000');
 $('#celular').mask('(00) 0000-00000');
 $('#dataNascimento').mask('00-00-0000');
+$('#dataInicio').mask('00-00-0000');
+$('#dataFim').mask('00-00-0000');
